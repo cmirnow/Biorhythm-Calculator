e6d1e3c539cd2c279deefcb1e7673f197f33d1cf
rishard <info@masterpro.ws>
2020-09-28 17:25:32 +0300

    code improvement

--- README.md
+++ README.md
@@ -3,11 +3,10 @@
 To get arrays of dates and biorhythm values, call these methods:
 
 ```bash
-values = BiorhythmsCalc.get_values(params[:birthday])
-dates = BiorhythmsCalc.get_dates
+values = BiorhythmsCalc.get_values_and_dates(params[:birthday])[0]
+dates = BiorhythmsCalc.get_values_and_dates(params[:birthday])[1]
 flash.now[:notice] = BiorhythmsCalc.flash_message(params[:birthday])
 ```
 
 [Demo](https://masterpro.herokuapp.com/bio/index).
 [Description](https://masterpro.ws/biorhythm-calculator-ruby) (in Russian).
-
--- biorhythms_calc.rb
+++ biorhythms_calc.rb
@@ -3,28 +3,23 @@ class BiorhythmsCalc
     Integer(Date.today + x - birthday)
   end
 
-  def self.get_values(birthday)
+  def self.get_values_and_dates(birthday)
     values = []
     [23, 28, 33].each do |x|
-      values << BiorhythmsCalc.biorhythm_counter(x, Date.strptime(birthday, '%d-%m-%Y').to_date)
+      values << BiorhythmsCalc.biorhythm_counter(x, Date.strptime(birthday, '%d-%m-%Y').to_date)[0]
     end
-    values
-  end
-
-  def self.get_dates
-    arr = []
-    (-3..14).each do |i|
-      arr << (Date.today + i).to_formatted_s(:short)
-    end
-    arr.map { |e| "`#{e}`" }.join(', ')
+    dates = BiorhythmsCalc.biorhythm_counter(x = 23, Date.strptime(birthday, '%d-%m-%Y').to_date)[1].map { |e| "`#{e}`" }.join(', ')
+    [values, dates]
   end
 
   def self.biorhythm_counter(number_of_cycle_days, birthday)
-    arr = []
+    arr1 = []
+    arr2 = []
     (-3..14).each do |x|
-      arr << (Math.sin(2 * Math::PI * BiorhythmsCalc.day_counter(x, birthday) / number_of_cycle_days) * 100).round(2)
+      arr1 << (Math.sin(2 * Math::PI * BiorhythmsCalc.day_counter(x, birthday) / number_of_cycle_days) * 100).round(2)
+      arr2 << (Date.today + x).to_formatted_s(:short)
     end
-    arr
+    [arr1, arr2]
   end
 
   def self.flash_message(birthday)
