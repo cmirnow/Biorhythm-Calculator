dd0d9a35bd0a64cd0caaea5f343392db782a1212
rishard <info@masterpro.ws>
2020-10-06 02:19:46 +0300

    code improvement

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,23 +1,19 @@
 class ApplicationController < Sinatra::Base
   configure do
-    set :views, "app/views"
-    set :public_dir, "public"
+    set :views, 'app/views'
+    set :public_dir, 'public'
   end
 
-
-
-    get '/' do
-    erb :'index'
+  get '/' do
+    erb :index
   end
 
-    post '/' do
-      unless params[:birthday].nil?
-    arr = BiorhythmsCalc.get_values_and_dates(params[:birthday])
-    @values = arr[0]
-@dates = arr.delete_at(1)
+  post '/' do
+    unless params[:birthday].nil?
+      arr = BiorhythmsCalc.get_values_and_dates(params[:birthday])
+      @values = arr[0]
+      @dates = arr.delete_at(1)
+    end
+    erb :index
   end
-    erb :'index'
-  end
-  
 end
-
